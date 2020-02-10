4cbcacd88d8c514fc39026a4fb1a2c0743130a27
Samson Edward <40283109+samson2018@users.noreply.github.com>
2020-02-10 20:46:12 +0530

    Add files via upload

---
+++ DeleteContact.java
@@ -0,0 +1,28 @@
+package opentaps.contact;
+
+import org.testng.annotations.Test;
+import wrappers.OpentapsWrappers;
+
+
+public class DeleteContact extends OpentapsWrappers {
+
+	
+	@Test
+	public void deleteContact() throws Exception{
+		clickByLink("Contacts");
+		clickByLink("Find Contacts");
+		enterByXpath("(//input[@name='firstName'])[3]", "Gopinath");
+		clickByXpath("//button[contains(text(),'Find Contacts')]");
+		String sText = getTextByXpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a[1]");
+		clickByXpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a[1]");		
+		clickByLink("Deactivate Contact");
+		alertAccept();
+		clickByLink("Find Contacts");
+		enterByXpath("//label[contains(text(),'Lead ID:')]/following::input", sText);
+		clickByXpath("//button[contains(text(),'Find Contacts')]");
+		Thread.sleep(3000);
+		verifyTextContainsByXpath("//div[@class='x-paging-info']", "No records to display");
+	}
+	
+
+}
